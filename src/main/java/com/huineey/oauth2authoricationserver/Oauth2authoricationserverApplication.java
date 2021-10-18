950ad66d382fd9bd25712e0c5f8c576dadf9152b
dahuin-1 <dawhite01@naver.com>
2021-10-18 18:38:34 +0900

    first commit

---
+++ src/main/java/com/huineey/oauth2authoricationserver/Oauth2authoricationserverApplication.java
@@ -0,0 +1,13 @@
+package com.huineey.oauth2authoricationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2authoricationserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2authoricationserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huineey/oauth2authoricationserver/Oauth2authoricationserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huineey.oauth2authoricationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2authoricationserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
